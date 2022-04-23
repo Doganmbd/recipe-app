ef029c17c852714e87bd048a72706e3f44e9c24a
DMB29 <mbilaldogan90@gmail.com>
2022-04-23 16:27:26 +0300

    Nav, Logo, Menu, MenuLink, Hamburger

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React,{useState} from "react";
+import { Nav, Logo, Menu, MenuLink, Hamburger } from "./NavbarStyle";
 
 const Navbar = () => {
-  return (
-    <div>
-      <Link to="/">Login</Link>
-    </div>
-  )
-}
 
-export default Navbar
+  const [isOpen, setIsOpen] = useState(false)
+  console.log(isOpen);
+  return (
+    <Nav>
+      <Logo to="/home">
+        <i>{"<MBD>"}</i>
+        <span>MyRecipe</span>
+      </Logo>
+{/* hamburger kısmını Menu altına yazarsak hamburger aşağıda görünür. */}
+      <Hamburger onClick={()=> setIsOpen(!isOpen)} >
+        <span></span>
+        <span></span>
+        <span></span>
+      </Hamburger>
+      {/* styleComponent in en önemli özelliklerinden biri prop gönderebiliyoruz. */}
+      <Menu isOpen={isOpen}>
+        <MenuLink to="/about">About</MenuLink>
+
+        <MenuLink to="/github">Github</MenuLink>
+
+        <MenuLink to="/">Logout</MenuLink>
+      </Menu>
+
+
+    </Nav>
+  );
+};
+
+export default Navbar;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,9 @@
 import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 import Footer from '../components/footer/Footer'
 import Navbar from '../components/navbar/Navbar'
+import About from '../pages/about/About'
+import Details from '../pages/details/Details'
+import Home from '../pages/home/Home'
 import Login from '../pages/login/Login'
 
 const AppRouter = () => {
@@ -10,6 +13,9 @@ const AppRouter = () => {
         <Navbar/>
         <Routes>
             <Route path="/" element={<Login/>} />
+            <Route path="/home" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/details" element={<Details/>} />
 
 
 
