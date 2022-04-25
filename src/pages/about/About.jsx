550e71c5e4816741bfbbeebdd58fdacfc080553b
DMB29 <mbilaldogan90@gmail.com>
2022-04-25 18:56:34 +0300

    about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import {
+  AboutContainer,
+  HeaderContainer,
+  InfoContainer,
+  StyledImage,
+} from "./AboutStyle";
+import codingSvg from "../../assets/coding.svg";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <AboutContainer>
+    
+      <StyledImage src={codingSvg} />
+      <HeaderContainer>
+        <h1>
+          About FrontEnd Developer <span>Mustafa Bilal DOĞAN </span>
+        </h1>
+      </HeaderContainer>
+      <InfoContainer>
+        <h2>Hi, I'am Mustafa Bilal</h2>
+        <h3>I’m currently learning Full-Stack Development Languages.</h3>
+        <h4>
+          I've already known JS, ReactJS, NodeJS,SQL,
+           Hibernate .
+        </h4>
+        <h2>
+          <a href="mailto:doganmbdogan@gmail.com">Send email</a> :
+          doganmbdogan@gmail.com
+        </h2>
+      </InfoContainer>
+    </AboutContainer>
+  );
+};
 
-export default About
+export default About;
