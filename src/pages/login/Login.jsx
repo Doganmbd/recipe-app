20f961e82771eb24f5bde56cf4998d425359f997
DMB29 <mbilaldogan90@gmail.com>
2022-04-25 17:48:15 +0300

    Login create

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { FormContainer, Header, LoginContainer, StyledButton, StyledForm, StyledImg, StyledInput } from './LoginStyle'
+import mealSvg from "../../assets/meal.svg"
 
 const Login = () => {
+
+
+  const handleSubmit = (e)=> {
+    e.preventDefault()
+    window.location.href = "/home"
+
+    // sayfa da login işlemlerini yapmadan başka sayfaya geçilmesin.
+    
+   
+  }
+
   return (
-    <div>Login</div>
+    <LoginContainer>
+      <FormContainer>
+      <StyledImg src={mealSvg} />
+        <Header>
+          {"<M.B.D>"}Recipe
+        </Header>
+        <StyledForm onSubmit={handleSubmit} >
+          <StyledInput type="text" placeholder="username" required />
+          <StyledInput type="password" placeholder="password" required/>
+          <StyledButton type="submit" >LOGIN</StyledButton>
+        </StyledForm>
+
+        
+      </FormContainer>
+
+    </LoginContainer>
   )
 }
 
