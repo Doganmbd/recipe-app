e12405765650ce7b465b296104ac2cffe426ac13
DMB29 <mbilaldogan90@gmail.com>
2022-04-25 18:30:37 +0300

    github and password

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,34 +1,46 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { Nav, Logo, Menu, MenuLink, Hamburger } from "./NavbarStyle";
 
-
-
 const Navbar = () => {
-
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   console.log(isOpen);
+
+  const [github, setGithub] = useState("");
+
   return (
     <Nav>
       <Logo to="/home">
         <i>{"<MBD>"}</i>
         <span>MyRecipe</span>
       </Logo>
-{/* hamburger kısmını Menu altına yazarsak hamburger aşağıda görünür. */}
-      <Hamburger onClick={()=> setIsOpen(!isOpen)} >
+      {/* hamburger kısmını Menu altına yazarsak hamburger aşağıda görünür. */}
+      <Hamburger onClick={() => setIsOpen(!isOpen)}>
         <span></span>
         <span></span>
         <span></span>
       </Hamburger>
       {/* styleComponent in en önemli özelliklerinden biri prop gönderebiliyoruz. */}
       <Menu isOpen={isOpen}>
-        <MenuLink to="/about" onClick={()=> setIsOpen(!isOpen)} >About</MenuLink>
+        <MenuLink to="/about" onClick={() => setIsOpen(!isOpen)}>
+          About
+        </MenuLink>
 
-        <MenuLink to="/github"  >Github</MenuLink>
+        <MenuLink
+          onClick={() =>
+            setGithub((window.location.href = "https://github.com/Doganmbd"))
+          }
+          to={github}
+          target="_blank"
+        >
+          Github
+        </MenuLink>
+        {/* target="_blank" is a special keyword that will open links in a new tab every time.
+        target="blank" will open the first-clicked link in a new tab, but any future links that share target="blank" will open in that same newly-opened tab. */}
 
-        <MenuLink to="/" onClick={()=> setIsOpen(!isOpen)}>Logout</MenuLink>
+        <MenuLink  onClick={() => setIsOpen(!isOpen)} onMouseUp={()=> sessionStorage.clear()} to="/">
+          Logout
+        </MenuLink>
       </Menu>
-
-
     </Nav>
   );
 };
